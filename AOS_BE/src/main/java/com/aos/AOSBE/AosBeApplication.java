31f6837b4aa1651a9892691eb629fde466cb1572
lllloc123h <locbhps39108@gmail.com>
2025-05-20 16:10:42 +0700

    Initial commit of All-in-One Store project (Backend)

---
+++ AOS_BE/src/main/java/com/aos/AOSBE/AosBeApplication.java
@@ -0,0 +1,13 @@
+package com.aos.AOSBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AosBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AosBeApplication.class, args);
+	}
+
+}
---
+++ AOS_BE/src/test/java/com/aos/AOSBE/AosBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aos.AOSBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AosBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
