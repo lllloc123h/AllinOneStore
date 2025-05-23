2f6149b3f00c1b3ec20e5f6614dce23cdfe120f1
khannps39199 <khannps39199@gmail.com>
2025-05-23 12:45:36 +0700

    add annotation @Service for each service file

--- AOS_BE/src/main/java/com/aos/AOSBE/Service/AccountsService.java
+++ AOS_BE/src/main/java/com/aos/AOSBE/Service/AccountsService.java
@@ -1,17 +1,12 @@
 package com.aos.AOSBE.Service;
 
 import java.util.List;
-
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.aos.AOSBE.Entity.*;
 import com.aos.AOSBE.Repository.*;
 import java.util.*;
-
-
+import org.springframework.stereotype.Service;
 @Service
 public class AccountsService {
 	@Autowired
--- AOS_BE/src/main/java/com/aos/AOSBE/Service/TraceXeActionMapService.java
+++ AOS_BE/src/main/java/com/aos/AOSBE/Service/TraceXeActionMapService.java
@@ -2,12 +2,12 @@ package com.aos.AOSBE.Service;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.aos.AOSBE.Entity.*;
 import com.aos.AOSBE.Repository.*;
 import java.util.*;
+import org.springframework.stereotype.Service;
+@Service
 public class TraceXeActionMapService {
 	@Autowired
     private TraceXeActionMapRepository traceTraceXeActionMapRepository;
