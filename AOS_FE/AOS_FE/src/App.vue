48065bd150e516d5aa42d9d03dc7fb3cec411b22
lllloc123h <locbhps39108@gmail.com>
2025-06-03 16:49:13 +0700

    create Register

--- AOS_FE/AOS_FE/src/App.vue
+++ AOS_FE/AOS_FE/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import ImageUpload from './components/Module/ImageUpload.vue';
 import Login from './components/Module/Login.vue';
+import Register from './components/Module/Register.vue';
 </script>
 
 <template>
   <router-view />
-  <Login></Login>
+  <Register/>
 </template>
