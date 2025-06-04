664f0a6b1c1d46e123cf8552a25a252328ce6754
tloc001 <locthps39267@gmail.com>
2025-06-04 08:50:38 +0700

    swap war to jar
    swap war to jar

--- AOS_BE/src/main/java/com/aos/AOSBE/AosBeApplication.java
+++ AOS_BE/src/main/java/com/aos/AOSBE/AosBeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AosBeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AosBeApplication.class, args);
-	}
+		SpringApplication.run(AosBeApplication.class, args);	}
 
 }
