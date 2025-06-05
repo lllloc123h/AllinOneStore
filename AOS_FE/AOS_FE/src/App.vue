9918428fc7c03dc43e6836e3cf11197f1e42ad3b
Wanderer674 <huynpps39177@gmail.com>
2025-06-05 11:23:38 +0700

    Code UI Shipping Address

--- AOS_FE/AOS_FE/src/App.vue
+++ AOS_FE/AOS_FE/src/App.vue
@@ -2,8 +2,10 @@
 import ImageUpload from './components/Module/ImageUpload.vue';
 import Login from './components/Module/Login.vue';
 import Register from './components/Module/Register.vue';
+import ShippingAddress from './components/Module/ShippingAddress.vue';
 </script>
 
 <template>
   <router-view />
+  <shipping-address/>
 </template>
