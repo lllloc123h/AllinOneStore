cbdac5ed03d97d9420ef637a8100fff2e0d47de9
khannps39199 <khannps39199@gmail.com>
2025-06-08 10:04:31 +0700

    config .env and apply .env to application.properties

--- AOS_BE/src/main/java/com/aos/AOSBE/AosBeApplication.java
+++ AOS_BE/src/main/java/com/aos/AOSBE/AosBeApplication.java
@@ -3,10 +3,27 @@ package com.aos.AOSBE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class AosBeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AosBeApplication.class, args);	}
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
+		System.setProperty("FACEBOOK_CLIENT_ID", dotenv.get("FACEBOOK_CLIENT_ID"));
+		System.setProperty("FACEBOOK_CLIENT_SECRET", dotenv.get("FACEBOOK_CLIENT_SECRET"));
+
+		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+		SpringApplication.run(AosBeApplication.class, args);
+	}
 
 }
