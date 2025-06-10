639ad773145d869f2caf7c3398bb5f2fb0d7b0e8
khannps39199 <khannps39199@gmail.com>
2025-06-10 13:24:33 +0700

    add e-wallet

--- AOS_BE/src/main/java/com/aos/AOSBE/Mapper/ProductItemsMapper.java
+++ AOS_BE/src/main/java/com/aos/AOSBE/Mapper/ProductItemsMapper.java
@@ -1,53 +1,30 @@
 package com.aos.AOSBE.Mapper;
 
-import java.time.LocalDateTime;
-import com.aos.AOSBE.DTOS.*;
-import com.aos.AOSBE.Entity.*;
-import com.aos.AOSBE.Service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.aos.AOSBE.DTOS.ProductItemsDTOS;
+import com.aos.AOSBE.Entity.ProductItems;
+import com.aos.AOSBE.Service.BaseProductsService;
+
 @Component
 public class ProductItemsMapper {
 	@Autowired
 	private BaseProductsService baseProductsService;
-	
+
 	public ProductItemsDTOS mapper(ProductItems entity) {
-		return new ProductItemsDTOS(
-				    entity.getId(),
-				    entity.getName(),
-				    entity.getCost(),
-				    entity.getPrice(),
-				    entity.isPromote(),
-				    entity.getTurnBuy(),
-				    entity.getDescription(),
-				    entity.getSku(),
-				    entity.getSafetyStock(),
-				    entity.getQty(),
-				    entity.getSellStart(),
-				    entity.getSellEnd(),
-				    entity.getCreatedAt(),
-				    entity.getUpdatedAt(),
-				    entity.getBaseProducts().getId()
-			);
+		return new ProductItemsDTOS(entity.getId(), entity.getName(), entity.getCost(), entity.getPrice(),
+				entity.isPromote(), entity.getTurnBuy(), entity.getDescription(), entity.getSku(),
+				entity.getSafetyStock(), entity.getQty(), entity.getSellStart(), entity.getSellEnd(),
+				entity.getCreatedAt(), entity.getUpdatedAt(), entity.getBaseProducts());
 	}
+
 	public ProductItems mapperToObject(ProductItemsDTOS entity) {
-		return new ProductItems(
-					entity.getId(),
-					entity.getName(),
-					entity.getCost(),
-					entity.getPrice(),
-					entity.isPromote(),
-					entity.getTurnBuy(),
-					entity.getDescription(),
-					entity.getSku(),
-					entity.getSafetyStock(),
-					entity.getQty(),
-					entity.getSellStart(),
-					entity.getSellEnd(),
-					entity.getCreatedAt(),
-					entity.getUpdatedAt(),
-					baseProductsService.baseProductsFindById(entity.getBaseProducts()).orElse(null)
-			);
+		return new ProductItems(entity.getId(), entity.getName(), entity.getCost(), entity.getPrice(),
+				entity.isPromote(), entity.getTurnBuy(), entity.getDescription(), entity.getSku(),
+				entity.getSafetyStock(), entity.getQty(), entity.getSellStart(), entity.getSellEnd(),
+				entity.getCreatedAt(), entity.getUpdatedAt(),
+				baseProductsService.baseProductsFindById(entity.getBaseProducts().getId()).orElse(null));
 	}
-	
+
 }
