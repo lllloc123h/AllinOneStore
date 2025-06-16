ce075b6bd39b0d4e2cc0453df1d98890e30ae5ad
khannps39199 <khannps39199@gmail.com>
2025-06-16 14:57:46 +0700

    Update AosBeApplicationTests.java

--- AOS_BE/src/test/java/com/aos/AOSBE/AosBeApplicationTests.java
+++ AOS_BE/src/test/java/com/aos/AOSBE/AosBeApplicationTests.java
@@ -1,10 +1,27 @@
 package com.aos.AOSBE;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootTest
+
 class AosBeApplicationTests {
+	@BeforeAll
+	static void loadEnv() {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+	}
 
 	@Test
 	void contextLoads() {
