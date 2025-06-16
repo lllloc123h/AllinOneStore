c3829c8604c80da3e842b12a7b95f6907d7db5cd
khannps39199 <khannps39199@gmail.com>
2025-06-16 13:51:36 +0700

    add cicd build

--- AOS_BE/src/main/java/com/aos/AOSBE/Repository/OrderItemsRepository.java
+++ AOS_BE/src/main/java/com/aos/AOSBE/Repository/OrderItemsRepository.java
@@ -1,11 +1,12 @@
 package com.aos.AOSBE.Repository;
 
-import com.aos.AOSBE.Entity.OrderItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.aos.AOSBE.Entity.OrderItems;
+
 @Repository
 public interface OrderItemsRepository extends JpaRepository<OrderItems, Integer> {
-    // Add custom query methods here if needed
-    List<OrderItems> findByOrdersId(Integer orderId);
+	// Add custom query methods here if needed
+	List<OrderItems> findByOrdersId(Integer orderId);
 }
