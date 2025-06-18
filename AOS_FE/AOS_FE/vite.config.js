a4f45248996573f78dec4aa5d077d526005cdb3c
tloc001 <locthps39267@gmail.com>
2025-06-18 19:16:16 +0700

    add file to production

--- AOS_FE/AOS_FE/vite.config.js
+++ AOS_FE/AOS_FE/vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"/"
+  base: '/AllinOneStore/', // <-- đúng tên repo của bạn
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
