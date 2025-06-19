7cf0556342fcec675317a1a6e77923fe309c8edb
tloc001 <locthps39267@gmail.com>
2025-06-19 08:01:22 +0700

    fix base

--- AOS_FE/AOS_FE/vite.config.js
+++ AOS_FE/AOS_FE/vite.config.js
@@ -4,23 +4,23 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/AllinOneStore/', // Tên repository của bạn
+  base: '', // Tên repository của bạn
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    minify: 'terser', // Minify code cho production
-    sourcemap: false, // Tắt sourcemap cho production
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['vue'],
-          antd: ['ant-design-vue'],
-          utils: ['axios', 'bootstrap']
-        }
-      }
-    }
+    // // minify: 'terser', // Minify code cho production
+    // sourcemap: false, // Tắt sourcemap cho production
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: {
+    //       vendor: ['vue'],
+    //       antd: ['ant-design-vue'],
+    //       utils: ['axios', 'bootstrap']
+    //     }
+    //   }
+    // }
   },
-  define: {
-    __VUE_PROD_DEVTOOLS__: false, // Tắt devtools trong production
-  }
+  // define: {
+  //   __VUE_PROD_DEVTOOLS__: false, // Tắt devtools trong production
+  // }
 })
