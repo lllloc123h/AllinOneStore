d227401846d83b5d30830978391f1604a1689240
tloc001 <locthps39267@gmail.com>
2025-06-19 09:33:50 +0700

    f

--- AOS_FE/AOS_FE/vite.config.js
+++ AOS_FE/AOS_FE/vite.config.js
@@ -4,23 +4,23 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/AllinOneStore/AOS_FE/AOS_FE/dist/', // Tên repository của bạn
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    // // minify: 'terser', // Minify code cho production
-    // sourcemap: false, // Tắt sourcemap cho production
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks: {
-    //       vendor: ['vue'],
-    //       antd: ['ant-design-vue'],
-    //       utils: ['axios', 'bootstrap']
-    //     }
-    //   }
-    // }
-  },
-  // define: {
-  //   __VUE_PROD_DEVTOOLS__: false, // Tắt devtools trong production
-  // }
+  base: '/AllinOneStore/', // Tên repository của bạn
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets',
+  //   // // minify: 'terser', // Minify code cho production
+  //   // sourcemap: false, // Tắt sourcemap cho production
+  //   // rollupOptions: {
+  //   //   output: {
+  //   //     manualChunks: {
+  //   //       vendor: ['vue'],
+  //   //       antd: ['ant-design-vue'],
+  //   //       utils: ['axios', 'bootstrap']
+  //   //     }
+  //   //   }
+  //   // }
+  // },
+  // // define: {
+  // //   __VUE_PROD_DEVTOOLS__: false, // Tắt devtools trong production
+  // // }
 })
