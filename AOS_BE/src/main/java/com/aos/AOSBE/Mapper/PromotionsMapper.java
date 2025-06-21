346bbe4e49eeb60df6b2cbbbec38023044911dca
khannps39199 <khannps39199@gmail.com>
2025-06-21 15:42:14 +0700

    update

--- AOS_BE/src/main/java/com/aos/AOSBE/Mapper/PromotionsMapper.java
+++ AOS_BE/src/main/java/com/aos/AOSBE/Mapper/PromotionsMapper.java
@@ -1,47 +1,26 @@
 package com.aos.AOSBE.Mapper;
 
-import java.time.LocalDateTime;
-import com.aos.AOSBE.DTOS.*;
-import com.aos.AOSBE.Entity.*;
-import com.aos.AOSBE.Service.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.aos.AOSBE.DTOS.PromotionsDTOS;
+import com.aos.AOSBE.Entity.Promotions;
+
 @Component
 public class PromotionsMapper {
-	
+
 	public PromotionsDTOS mapper(Promotions entity) {
-		return new PromotionsDTOS(
-				    entity.getId(),
-				    entity.getName(),
-				    entity.getDescription(),
-				    entity.getType(),
-				    entity.getDiscountType(),
-				    entity.getDiscountValue(),
-				    entity.getComboPrice(),
-				    entity.getUsageLimit(),
-				    entity.getStartAt(),
-				    entity.getEndAt(),
-				    entity.isActive(),
-				    entity.getCreatedAt(),
-				    entity.getUpdatedAt()
-			);
+		return new PromotionsDTOS(entity.getId(), entity.getName(), entity.getDescription(), entity.getType(),
+				entity.getDiscountType(), entity.getDiscountValue() != null ? entity.getDiscountValue() : 0.0,
+				entity.getComboPrice() != null ? entity.getComboPrice() : 0.0, entity.getUsageLimit(),
+				entity.getStartAt(), entity.getEndAt(), entity.isActive(), entity.getCreatedAt(),
+				entity.getUpdatedAt());
 	}
+
 	public Promotions mapperToObject(PromotionsDTOS entity) {
-		return new Promotions(
-					entity.getId(),
-					entity.getName(),
-					entity.getDescription(),
-					entity.getType(),
-					entity.getDiscountType(),
-					entity.getDiscountValue(),
-					entity.getComboPrice(),
-					entity.getUsageLimit(),
-					entity.getStartAt(),
-					entity.getEndAt(),
-					entity.isActive(),
-					entity.getCreatedAt(),
-					entity.getUpdatedAt()
-			);
+		return new Promotions(entity.getId(), entity.getName(), entity.getDescription(), entity.getType(),
+				entity.getDiscountType(), entity.getDiscountValue(), entity.getComboPrice(), entity.getUsageLimit(),
+				entity.getStartAt(), entity.getEndAt(), entity.isActive(), entity.getCreatedAt(),
+				entity.getUpdatedAt());
 	}
-	
+
 }
