2d12cebc9716722d5464a57b5add883314d51fcd
khannps39199 <khannps39199@gmail.com>
2025-07-08 22:58:23 +0700

    resolve conflict

--- AOS_BE/src/main/java/com/aos/AOSBE/Repository/PromotionProductsRepository.java
+++ AOS_BE/src/main/java/com/aos/AOSBE/Repository/PromotionProductsRepository.java
@@ -2,26 +2,16 @@ package com.aos.AOSBE.Repository;
 
 import java.util.List;
 
-<<<<<<< Updated upstream
-import org.springframework.data.jpa.repository.JpaRepository;
-=======
-import com.aos.AOSBE.Entity.Promotions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
->>>>>>> Stashed changes
 import org.springframework.stereotype.Repository;
 
 import com.aos.AOSBE.Entity.PromotionProducts;
 
 @Repository
 public interface PromotionProductsRepository extends JpaRepository<PromotionProducts, Integer> {
-    List<PromotionProducts> findByProductItems_Id(int productItemId);
-<<<<<<< Updated upstream
-}
-=======
+	List<PromotionProducts> findByProductItems_Id(int productItemId);
 
-    @Query("SELECT pp FROM PromotionProducts pp WHERE pp.promotions.id = ?1 ")
-    List<PromotionProducts> findPromotionProductsByPromotionId(int promotionId);
+	@Query("SELECT pp FROM PromotionProducts pp WHERE pp.promotions.id = ?1 ")
+	List<PromotionProducts> findPromotionProductsByPromotionId(int promotionId);
 }
-
->>>>>>> Stashed changes
