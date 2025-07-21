dfb14e4747beaf1e5e06240b8b1d93d15a6fea33
khannps39199 <khannps39199@gmail.com>
2025-07-21 12:47:58 +0700

    Update UpdateProfileDTO.java

--- AOS_BE/src/main/java/com/aos/AOSBE/DTOS/UpdateProfileDTO.java
+++ AOS_BE/src/main/java/com/aos/AOSBE/DTOS/UpdateProfileDTO.java
@@ -4,13 +4,18 @@ import lombok.Data;
 
 @Data
 public class UpdateProfileDTO {
-    private String fullname;
-    private String email;
-    private String phone;
-    private String avatar;
-    private String address;
+	private String fullname;
+	private String email;
+	private String phone;
+	private String avatarUrl;
+	private String address;
 
-    private Integer provinceName;
-    private Integer districtName;
-    private String wardName;
+	private Integer provinceName;
+	private Integer districtName;
+	private String wardName;
+
+	private String province;
+	private String district;
+	private String ward;
+	private String street;
 }
